c62f6968683a80f5a30f66fef6c7f17e37eb2050
mark <mark@secondbureau.com>
2011-08-09 14:43:19 +0800

    #790 add user login/logout information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_filter :redirect_root_unless_is_current_user
+  
   def index
   end
   
@@ -9,6 +11,18 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find params[:id]
+  end
+  
+  def update
+    User.update(params[:id], params[:user])
+    redirect_to main_app.user_path(current_user), :notice => "Update information successfully!"
+  end
+  
+private
+  def redirect_root_unless_is_current_user
+    unless current_user == User.find(params[:id])
+      redirect_to main_app.root_path, :notice => "Please log in to continue!"
+    end
   end
 
 end
