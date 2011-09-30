460032300eab9f28d89ac1004d34cb51a3a7b6bf
mark <mark@secondbureau.com>
2011-09-30 16:04:01 +0800

    manage roles in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   
   def index
     @latest_posts = Post.limit(5)
-    @latest_events = Event.limit(5)
   end
   
   def show
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,14 +15,8 @@ class Ability
       case user.role.name.to_sym
       when :admin
         as_admin user
-      when :officer
-        as_officer user
       when :member
         as_member user
-      when :user
-        as_user user
-      when :guest
-        as_guest user
       end
     end
 
@@ -37,45 +31,12 @@ class Ability
   end
   
   #
-  # what officer can do
-  #
-  def as_officer user
-    can :manage, Post
-    can :manage, Comment
-    can :manage, User
-    
-    admin_id = Role.where('name = ?', 'admin').first.id;
-    officer_id = Role.where('name = ?', 'officer').first.id;
-    
-    cannot :manage, User, :role_id => [admin_id, officer_id]
-    can :update, User, :id => user.id
-    can :read, User, :role_id => officer_id
-    can :read, Role
-    cannot :read, Role, :id => [admin_id, officer_id]
-  end
-  
-  #
   # what member can do
   #
   def as_member user
     can :read, User, :id => user.id
     can :update, User, :id => user.id
     can :read, Comment, :user_id => user.id
-  end
-  
-  #
-  # what user can do
-  #
-  def as_user user
-    can :update, User, :id => user.id
-    can :read, Comment, :user_id => user.id
-  end
-  
-  #
-  # what guest can do
-  #
-  def as_guest user
-    cannot :access, :rails_admin
   end
   
 end
