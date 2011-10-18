e2c60044a0d0a48e9674651f9eb427ef8b1bb7b3
mark <mark@secondbureau.com>
2011-10-18 12:01:51 +0800

    #845 Stylize the account link and search link and the search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,14 @@ class SearchController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    condition = {:title_or_content_contains => params[:search]}
-    @posts = Post.search(condition).all
-    @events = Event.search(condition).all
+    if params[:search]
+      condition = {:title_or_content_contains => params[:search]}
+      @posts = Post.search(condition).all
+      @events = Event.search(condition).all
+    else
+      @posts = []
+      @events = []
+    end
   end
 
 end
