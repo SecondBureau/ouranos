f837b491a45b7f596be7af5fbee30bdf4469be93
mark <mark@secondbureau.com>
2011-11-08 10:25:38 +0800

    #871 - Fix bug for link title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def link_to_switch_locale( locale )
     uparams = params.dup
     uparams[:locale] = locale
-    link_to image_tag("flags/#{locale}.png"), main_app.url_for( uparams.merge(:only_path => true ))
+    link_to image_tag("flags/#{locale}.png"), main_app.url_for( uparams.merge(:only_path => true )), {:title => locale}
   end
   
 end
