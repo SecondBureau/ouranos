27716b7f534509b08d693c157cff7ca23131da64
mark <mark@secondbureau.com>
2011-11-09 12:03:22 +0800

    #871 - Fix bugs for the typesetting

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,5 +1,11 @@
 class Ckeditor::Picture < Ckeditor::Asset
   mount_uploader :data, CkeditorPictureUploader, :mount_on => :data_file_name
+  
+  has_attached_file :data,
+              :storage => :s3,
+              :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+              :path => ":class/:id/:basename.:extension",
+              :styles => {}
 	
 	def url_content
 	  url(:content)
