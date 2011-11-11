fc02c959023ebbeae0d3251d9a808b37a37050af
Gilles Crofils <gilles@secondbureau.com>
2011-11-11 15:55:40 +0800

    #872 - remove hard coded params

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,11 @@
 class Image < ActiveRecord::Base
   has_attached_file :image, :styles => { :banner_image => "620x190>", :thumb => "50x50>" },
     :storage => :s3,
-    :bucket => "jzwhdemo" ,
+    :bucket => $s3_bucket ,
     :s3_credentials => {
-      :access_key_id => "AKIAJ6DZNIC4O23W6MJA" ,
-      :secret_access_key => "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"
+      :access_key_id => $s3_access_key_id  ,
+      :secret_access_key => $s3_secret_access_key
     }
   belongs_to :image_owner , :polymorphic => true
 end
+
