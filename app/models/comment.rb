f2decd0f566449e8b0c378d3debf0a6a38af57bd
mark <mark@secondbureau.com>
2011-11-14 15:53:57 +0800

    #875 - Clean up

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
   
+  validates_presence_of :content
+  
   default_scope :order => 'created_at DESC'
   
 end
