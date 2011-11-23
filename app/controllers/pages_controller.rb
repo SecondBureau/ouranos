32a7f0888d64cff507bbea35a72248ae58dc013a
mark <mark@secondbureau.com>
2011-11-23 14:38:46 +0800

    #Improvement - All statis pages could be shown to all users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
 
-  before_filter :authenticate_user!, :is_membership_expired?, :except => :index
-  before_filter :check_member_confirmation
-
   def index
     @latest_posts = PostDecorator.decorate(Post.where(:is_pinned => false).limit(5))
     @pinned_posts = PostDecorator.decorate(Post.where(:is_pinned => true))
