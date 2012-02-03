c749e1b12baab8924d6df927999ade29f0e86df3
Gilles Crofils <gilles@secondbureau.com>
2012-02-03 16:04:17 +0800

    improved performances

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ Ouranos::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -49,5 +50,5 @@ Ouranos::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
+
 end
