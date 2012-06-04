c1ac6f4e230994b6b8b92fbf249b7976a78043ad
Romain Binaux <romain@secondbureau.com>
2012-06-04 14:54:10 +0800

    Try fix bug

--- db/migrate/20120522090537_add_token_column_to_user.rb
+++ db/migrate/20120522090537_add_token_column_to_user.rb
@@ -1,10 +1,5 @@
 class AddTokenColumnToUser < ActiveRecord::Migration
-  def self.up
-    change_table :users do |t|
-      t.token_authenticatable
-    end
-  end
-  def self.down
-    t.remove :authentication_token
+  def change
+    add_column :users, :token_authenticatable, :string
   end
 end
