26d2f900384e37063e6fe8e6011d2c5831c40aba
Romain Binaux <romain@secondbureau.com>
2012-06-13 15:08:28 +0800

    config mail

--- config/environment.rb
+++ config/environment.rb
@@ -7,17 +7,11 @@ Ouranos::Application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
-#  :enable_starttls_auto => true,
-#  :address => 'caffeine.2bu.ro',
-#  :port => 25,
-#  :domain => 'localhost',
-#  :authentication => :plain,
-#  :user_name => 'smtpsender@2bu.ro',
-#  :password => 'secret11'
-  :address => 'smtp.gmail.com',
-  :port => 587,
+  :enable_starttls_auto => true,
+  :address => 'caffeine.2bu.ro',
+  :port => 25,
   :domain => 'localhost',
   :authentication => :plain,
-  :user_name => 'romain.binaux@gmail.com',
-  :password => 'sonnytgmail48'
+  :user_name => 'smtpsender@2bu.ro',
+  :password => 'secret11'
 }
