5b8fc90a9220a5b839ecf5d59c78b3f916fbdf4a
Gilles Crofils <gilles@secondbureau.com>
2012-10-07 18:20:45 +0800

    enabled initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module Ouranos
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     
 
     # Version of your assets, change this if you want to expire all your assets
