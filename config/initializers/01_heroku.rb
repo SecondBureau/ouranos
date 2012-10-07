426f695e8bd561f2ec1c8cc28a98ef750a847f86
Gilles Crofils <gilles@secondbureau.com>
2012-10-07 14:43:44 +0800

    added GA tracker

--- config/initializers/01_heroku.rb
+++ config/initializers/01_heroku.rb
@@ -1,3 +1,4 @@
 $smtp_settings            = ENV['SMTP_SETTINGS']
 $mailchimp_api_key        = ENV['MAILCHIMP_API_KEY'] || "fadc53f22112beef63242dbcab00d386-us6"
 $mailchimp_group_id       = ENV['MAILCHIMP_GROUP_ID'] || "00268b20d2"
+$ga_tracker_id            = ENV['GA_TRACKER1'] || 'UA-xxxxxx-x'
