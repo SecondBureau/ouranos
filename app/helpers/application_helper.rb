7aabe9a78ef2c941eb03d08b3ae137b631198335
Gilles Crofils <gilles@secondbureau.com>
2012-10-07 18:57:40 +0800

    bugfix migrate to refinery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 module ApplicationHelper
 
   def banner_image_url
-    if Rails.env.production? && setting && setting.images[Random.rand(setting.images.length)]
-      rand_index = Random.rand(setting.images.length)
-      setting.images[rand_index].image.url(:banner_image)
-    else
       "home_image.jpg"
-    end
   end
   
   def fullname (user)
